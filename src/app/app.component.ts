65ed4eef69d066b8d7a29a0cc482fe6e39707d95
Cesar Martinez <cesarwamartinez@gmail.com>
2025-05-30 15:41:20 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { lucideAlignCenter } from '@ng-icons/lucide';
+
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  viewProviders: [provideIcons({lucideAlignCenter})]
 })
 export class AppComponent {
   title = 'firmas';
